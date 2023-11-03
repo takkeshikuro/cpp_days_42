19ca5bcb1d774948621281e7967fbe0c8a487c7f
takkeshikuro <takeshi.27@outlook.fr>
2023-11-03 14:27:23 +0100

    fix makefile

--- CPP_01/ex05/include/Harl.hpp
+++ CPP_01/ex05/include/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: keshikuro <keshikuro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 06:30:22 by tmorikaw          #+#    #+#             */
-/*   Updated: 2023/11/03 12:40:30 by keshikuro        ###   ########.fr       */
+/*   Updated: 2023/11/03 14:25:47 by keshikuro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 # define HARL_HPP
 
 #include <iostream>
-#include <map>
 
 class Harl
 {
-	public : 
+	public :
 		Harl();
 		~Harl();
-
 		void complain(std::string level);
+
 	private :
 		void debug(); //infos contextuelles
 		void info(); // infos detailles
