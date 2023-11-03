459a405eaf12fcf46dca20db0300da8713423435
takkeshikuro <takeshi.27@outlook.fr>
2023-11-03 13:26:25 +0100

    cpp01 done

--- CPP_01/ex06/include/Harl.hpp
+++ CPP_01/ex06/include/Harl.hpp
@@ -5,29 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: keshikuro <keshikuro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 06:30:22 by tmorikaw          #+#    #+#             */
-/*   Updated: 2023/11/03 12:40:30 by keshikuro        ###   ########.fr       */
+/*   Created: 2023/11/03 12:58:14 by keshikuro         #+#    #+#             */
+/*   Updated: 2023/11/03 13:07:55 by keshikuro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
-# define HARL_HPP
+#define HARL_HPP
 
 #include <iostream>
-#include <map>
 
 class Harl
 {
 	public : 
 		Harl();
 		~Harl();
-
 		void complain(std::string level);
-	private :
+	
+    private :
 		void debug(); //infos contextuelles
 		void info(); // infos detailles
 		void warning(); // msg avertissement
 		void error(); // critique
 };
-
 #endif
--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: keshikuro <keshikuro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 06:42:41 by tmorikaw          #+#    #+#             */
-/*   Updated: 2023/11/03 12:55:23 by keshikuro        ###   ########.fr       */
+/*   Created: 2023/11/03 13:04:28 by keshikuro         #+#    #+#             */
+/*   Updated: 2023/11/03 13:25:35 by keshikuro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,51 @@
 void Harl::complain(std::string level) 
 {
 	void (Harl::*p_function[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	std::string levels[4] = {"debug", "info", "warning", "error"};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++)
 	{
 		if (level == levels[i])
-			return (this->*p_function[i])();
+		{
+			int ok = i;
+			while (ok < 4)
+			{
+				switch (ok) 
+				{
+					case 0:
+						(this->*p_function[ok])();
+						break;
+					case 1:
+						(this->*p_function[ok])();
+						break;
+					case 2:
+						(this->*p_function[ok])();
+						break;
+					case 3:
+						(this->*p_function[ok])();
+						break;
+				}
+				ok++;
+			}
+			return ;
+		}
 	}
-	std::cout << "No msg" << std::endl;
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	return ;
 }
 
-int main(void)
+int main(int ac, char **av)
 {
-	Harl harl;
-	harl.complain("error");
-	harl.complain("debug");
-	harl.complain("warning");
-	harl.complain("info");
-	harl.complain("ok");
+	if (ac == 2)
+	{
+		std::string level = av[1];
+		if (!level.empty())
+		{
+			Harl harl;
+			harl.complain(level);
+			return 0;
+		}
+	}
+	std::cout << "use ./HarlFilter with DEBUG/INFO/WARNING/ERROR " << std::endl;	
 	return 0;
 }
